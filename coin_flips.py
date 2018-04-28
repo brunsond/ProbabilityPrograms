98eb510e4f86f38df51b7100e64462984175f997
brunsond <37097298+brunsond@users.noreply.github.com>
2018-04-27 19:30:19 -0600

    Update coin_flips.py

--- coin_flips.py
+++ coin_flips.py
@@ -4,4 +4,13 @@ def coins_flips(n,m):
     if (n>m):
         pass
     else:
+        result_1 = 1
+        for i in range(0,n-k):
+            result_1 *= (n-i)
+        result_2 = 1
+        for i in range(1,n-k+1):
+            result_2 *= i
+        combins = result_1/result_2
+        p = combins*0.5**n
+        return p
         
