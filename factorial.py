41f0988fa32299d019e56dee1197996fa27d0654
brunsond <37097298+brunsond@users.noreply.github.com>
2018-04-27 19:09:21 -0600

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,8 +1,10 @@
 #Returns the factorial of an integer n.
 
-def factorial(n)
-    result = 1
-    for i in range(1,n+1):
-        result *= i
-    return result
-
+def factorial(n):
+    if (n<0):
+        pass
+    else:
+        result = 1
+        for i in range(1,n+1):
+            result *= i
+        return result
