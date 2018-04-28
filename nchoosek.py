577be54dff6ae317060c326c5afeedf4bfb60714
brunsond <37097298+brunsond@users.noreply.github.com>
2018-04-27 19:21:19 -0600

    Update nchoosek.py

--- nchoosek.py
+++ nchoosek.py
@@ -1,12 +1,16 @@
 #Returns the value of n choose k.
 
 def nchoosek(n,k):
-    result_1 = 1
-    for i in range(0,n-k):
-        result_1 *= (n-i)
-    result_2 = 1
-    for i in range(1,n-k+1):
-        result_2 *= i
-    return result_1/result_2
-    
+    if (n<0 or k<0):
+        pass
+    elif (n<k):
+        return 0
+    else:
+        result_1 = 1
+        for i in range(0,n-k):
+            result_1 *= (n-i)
+        result_2 = 1
+        for i in range(1,n-k+1):
+            result_2 *= i
+        return result_1/result_2
     
